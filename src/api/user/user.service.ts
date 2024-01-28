e421c92e1a41618019ff9344b5e32d472cd44ce4
aek <darkmift@gmail.com>
2024-01-28 02:04:01 +0200

    Update collection name in UserCrudService constructor

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -3,6 +3,6 @@ import MongoCrudService from '@/services/mongo-crud.service';
 
 export default class UserCrudService extends MongoCrudService<User> {
     constructor() {
-        super(UserModel, 'moderator_users');
+        super(UserModel, UserModel.collection.name);
     }
 }
